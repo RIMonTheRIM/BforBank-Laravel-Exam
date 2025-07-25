9a19e751f43eb81c638410c423e000a39bbb9ac2
Ihasiniravo "RIME <silverlion2424@gmail.com>
2025-07-25 01:52:38 +0000

    setup to create an admin

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,7 +73,7 @@ class RegisterController extends Controller
             'prenom' => $data['prenom'],
             'telephone' => $data['telephone'],
             'email' => $data['email'],
-            'role' => Role::Client->value,
+            'role' => Role::Gestionnaire->value,
             'password' => Hash::make($data['password']),
         ]);
     }
