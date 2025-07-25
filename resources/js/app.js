3c16b599ea609e117c71fd1de1e5b9311d13e750
Ihasiniravo "RIME <silverlion2424@gmail.com>
2025-07-25 01:51:25 +0000

    fixing vite not building css files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
-import '../css/accountchoice.css';
+
 import '../css/app.css';
+
+import '../css/accountchoice.css';
 import '../css/compteInfo.css';
-import '../css/dashboard.css';
 import '../css/gestionComptes.css';
 import '../css/gestionDemandes.css';
 import '../css/gestionTransactions.css';
-import '../css/home.css';
 import '../css/pdfHisto.css';
+
+import '../css/home.css';
+import '../css/dashboard.css';
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,15 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/home.css',
+                'resources/css/dashboard.css',
                 'resources/js/app.js',
+                'resources/css/accountchoice.css',
+                'resources/css/compteInfo.css',
+                'resources/css/gestionComptes.css',
+                'resources/css/gestionDemandes.css',
+                'resources/css/gestionTransactions.css',
+                'resources/css/pdfHisto.css'
             ],
             refresh: true,
         }),
